5cfde1118bd97d6bfb6c5dbbb8f806e0b5213a42
William Sterling <sterlinw@b19945.local>
2021-02-07 14:08:11 -0700

    Init Commit. Working MP3

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,42 @@
 import React, { Component } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 import logo from '../logo.png';
 import './App.css';
 
+const HIGHREZ_AUDIO_FILE = "music/RussianBlues.mp3";
+const COVER_IMAGE = "images/cover.jpeg"
+// const HIGHREZ_AUDIO_FILE = "music/RussianBlues.mp3"
+
 class App extends Component {
+
+  
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-ui fixed-top flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            MusicStreams
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+              <div className="card nft-card">
+                <div className="card-image">
+                  <img src={COVER_IMAGE} alt="cover image" />
+                </div>
+              <ReactAudioPlayer
+                // src="music/RussianBlues.mp3"
+                src={HIGHREZ_AUDIO_FILE}
+                // autoPlay
+                controls
+              />
               </div>
             </main>
           </div>
