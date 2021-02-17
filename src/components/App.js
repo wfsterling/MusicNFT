f0acb077512bda351f6615ee68a2cbc25fd4bf31
William Sterling <sterlinw@b19945.local>
2021-02-16 23:15:28 -0700

    Adding IPFS to sample and cover_image

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,9 @@ import Web3 from 'web3';
 
 // While learning REACT components, used the following
 // to run a list of local list of tokens using MetaMask account:
-const SAMPLE_AUDIO_FILE = "music/RussianBlues-sample.mp3";
+const SAMPLE_AUDIO_FILE = "https://ipfs.io/ipfs/QmUSeLArt2H7Pr1nsj4Md94tkjGyQoWWj2iRFhddSYfany";
 // const HIGHREZ_AUDIO_FILE = "music/RussianBlues-full.mp3";
-// const COVER_IMAGE = "images/concert/concert1.jpg";
+const TOKEN_COVER = "https://ipfs.io/ipfs/QmT6iGtffKzqjkgsVVi6jUytcecGLgwd9XjNjcmXcMU2UY";
 // const TOKEN_TITLE = "Russian Blues";
 // const TOKEN_ARTIST = "Nick, Sergey, and William";
 const TOKEN_PRICE = ".001";
@@ -94,7 +94,7 @@ class App extends Component {
                   <div key={i} className="nft-wrapper">
                     <MusicPreviewCard
                       sample={SAMPLE_AUDIO_FILE}
-                      cover={'images/concert/concert' + (i+1) + '.jpg'}
+                      cover={TOKEN_COVER}
                       title={token} 
                       artist = {token}
                       price = {TOKEN_PRICE}
